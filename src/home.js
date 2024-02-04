c8070e12aff1a868b6047bf7d0a4c3f80526555a
NNikoGG <geekgaming222@gmail.com>
2024-02-04 23:35:12 +0530

    Add homepage in JS

--- src/home.js
+++ src/home.js
@@ -1,7 +1,28 @@
 const createHomepage = () => {
     const content = document.querySelector('#content');
-    const pageContent = document.createElement('div');
-    pageContent.classList.add('page-content');
+    // const pageContent = document.createElement('div');
+    // pageContent.classList.add('page-content');
+    // content.appendChild(pageContent);
+    
+    const headline = document.createElement('h2');
+    headline.textContent = `Welcome to the Wasteland's Finest Culinary Haven!`;
+    content.appendChild(headline);
+
+    const subtext = document.createElement('h3');
+    subtext.textContent = 'Where Survival Meets Flavor!';
+    content.appendChild(subtext);
+
+    const info = document.createElement('div');
+    info.classList.add('info');
+    info.innerHTML = `<div class="address">
+                        <img src="../src/location.png" alt="">
+                        <p>Vault 101, Main Street, <br> Capital Wasteland</p>
+                    </div>
+                    <div class="hours">
+                        <img src="../src/clock.png" alt="">
+                        <p>Open Daily: 10:00 AM to 10:00 PM <br> Late Night Specials on Fridays and Saturdays: 10:00 PM to 2:00 AM</p>
+                    </div>`
+    content.appendChild(info);
 }
 
 export default createHomepage;
