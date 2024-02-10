5ea349d23a47966cec6db7e44a9d596b11cde70d
NNikoGG <geekgaming222@gmail.com>
2024-02-11 05:12:47 +0530

    Fix image file paths

--- src/contact.js
+++ src/contact.js
@@ -11,7 +11,7 @@ const createContactPage = () => {
     contactContainer.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d189.0243027587925!2d-74.01022474253145!3d40.70945576523445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a177d4bf5db%3A0x84e51f23e8c0a75c!2sMcDonald&#39;s!5e0!3m2!1sen!2sin!4v1707600777112!5m2!1sen!2sin" width="700" height="300" style="border:0;" allowfullscreen="" frameborder="0"
     aria-hidden="false"></iframe>
     <div class="message-headline-container">
-        <img src="../src/images/email.svg" alt="">
+        <img src="./images/email.svg" alt="">
         <h3 id="message-headline">Message Us</h3>
     </div>
     <form action="">
--- src/menu.js
+++ src/menu.js
@@ -9,22 +9,22 @@ const createMenuPage = () => {
     menuContainer.className = 'menu-container';
     content.appendChild(menuContainer);
     menuContainer.innerHTML = `<div class="item-1">
-                                    <img src="../src/images/deathclaw-wellington.png" alt="">
+                                    <img src="./images/deathclaw-wellington.png" alt="">
                                     <p>Deathclaw Wellington</p>
                                     <p>100 bottlecaps</p>
                                 </div>
                                 <div class="item-2">
-                                    <img src="../src/images/iguana-on-a-stick.png" alt="">
+                                    <img src="./images/iguana-on-a-stick.png" alt="">
                                     <p>Iguana on a Stick</p>
                                     <p>80 bottlecaps</p>
                                 </div>
                                 <div class="item-3">
-                                    <img src="../src/images/radscorpion-en-croute.png" alt="">
+                                    <img src="./images/radscorpion-en-croute.png" alt="">
                                     <p>Radscorpion En Croute</p>
                                     <p>110 bottlecaps</p>
                                 </div>
                                 <div class="item-4">
-                                    <img src="../src/images/firecap-soup.jpg" alt="">
+                                    <img src="./images/firecap-soup.jpg" alt="">
                                     <p>Firecap Soup</p>
                                     <p>90 bottlecaps</p>
                                 </div>`
